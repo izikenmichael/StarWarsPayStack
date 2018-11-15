7995e01b2c51f23518920587f367ccd758c66a47
izikenmichael <izikenmichael@gmail.com>
2018-11-15 22:08:59 +0100

    u
    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { SwMoviesService } from "./sw-movies/sw-movies.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = "CodeSandbox";
+export class AppComponent implements OnInit {
+  title = "starWarsLocal";
+
+  constructor(private moviesService: SwMoviesService) {}
+
+  ngOnInit(): void {
+    this.moviesService.getMoviesData();
+  }
 }
